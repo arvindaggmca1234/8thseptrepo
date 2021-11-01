9d204cd30a1d991240f79995cb2efffd37e53a3a
arvindaggmca1234 <90887043+arvindaggmca1234@users.noreply.github.com>
2021-10-31 22:32:37 -0400

    Add files via upload

---
+++ ScalaProject/src/main/scala/com.test/Test1.scala
@@ -0,0 +1,18 @@
+package main.scala
+
+object Test1 {
+
+    def main(args: Array[String]): Unit = {
+      val spark = SparkSession.builder
+        .appName("Word Count")
+        .config("spark.some.config.option", "some-value")
+        .getOrCreate()
+      val textFile = spark.sparkContext.textFile("s3n://myemrs3/test")
+      val counts = textFile.flatMap(line => line.split(" "))
+        .map(word => (word, 1))
+        .reduceByKey(_+_)
+      counts.collect()
+      // counts.saveAsTextFile("hdfs://...")
+    }
+
+}
